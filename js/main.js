21c0c3d9034779b65b831b9febe5ea31767f5cd3
alexeirechitskiy <slowspirit@gmail.com>
2023-01-30 15:02:57 +0300

    implemented minimal functionality

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 
 const input = document.querySelector(".input");
 const addButton = document.querySelector(".add-button");
-// const deleteButton = document.querySelectorAll(".delete-button");
 const tasksCheck = document.querySelector(".task__check");
-let workField = document.querySelector(".tasks");
+const workField = document.querySelector(".tasks");
+const taskElement = document.querySelector(".task");
+const tasksTitle = document.querySelector(".tasks__title");
+
 let tasksList = [];
 let idCounter = 0;
 
@@ -14,74 +16,71 @@ const clearInput = function () {
 
 const drawAnElement = function (task) {
 	const taskPost = `
-	<li class="task" id="${task.id}">
-	<input class="task__check" type="checkbox">
-	<p class="task__text">${task.task}</p>
-	<button class="delete-button">Delete</button>
-	</li>
+	  <li class="task" id="${task.id}">
+	  <p class="task__text">${task.task}</p>
+	  <button class="delete-button">Delete</button>
+	  </li>
 	`;
-
 	workField.innerHTML += taskPost;
 };
 
-const watcher = function () {
-	console.log(workField);
-	const tasks = document.querySelectorAll(".task");
-	console.log("tasks: ", tasks);
+const toggleDone = function () {
+	const taskText = document.querySelectorAll(".task__text");
+
+	for (let k = 0; k < taskText.length; k++) {
+		taskText[k].addEventListener("click", function () {
+			this.classList.toggle("done");
+
+			const idIemp = this.closest(".task").id;
+			console.log(idIemp);
+
+			tasksList[idIemp - 1].done = !tasksList[idIemp - 1].done;
+			console.log(idIemp, tasksList[idIemp - 1].done);
+			console.log(tasksList);
+		});
+	}
 };
 
-addButton.addEventListener("click", function () {
-	if (input.value) {
+const addTask = function () {
+	if (input.value != "") {
+		tasksTitle.classList.remove("hidden");
 		const newTask = {
 			id: idCounter + 1,
 			task: input.value,
+			done: false,
 		};
 		idCounter++;
 		tasksList.push(newTask);
-		console.log("tasksList: ", tasksList);
 		drawAnElement(newTask);
+
+		toggleDone();
 
 		// Deleting task from list
 		const deleteButton = document.querySelectorAll(".delete-button");
+
 		for (let i = 0; i < deleteButton.length; i++) {
 			deleteButton[i].addEventListener("click", function () {
-				console.log(deleteButton[i].closest(".task").id);
 				const taskId = deleteButton[i].closest(".task").id;
-				console.log("Block's id: ", taskId);
-				// tasksList.splice(taskId, taskId);
-				console.log("before: ", tasksList);
-
 				for (let j = 0; j < tasksList.length; j++) {
 					if (tasksList[j].id == taskId) {
-						console.log(
-							"I need to delete the task contains id:",
-							tasksList[j].id,
-							"Index of this task is: ",
-							j
-						);
 						tasksList.splice(j, 1);
-						// break;
 					}
-
-					// if (tasksList[j].id === taskId) {
-					// 	console.log("Delete this item:", tasksList[j]);
-					// }
 				}
-				console.log("after: ", tasksList);
-
 				deleteButton[i].closest(".task").remove();
+
+				if (tasksList.length === 0) {
+					tasksTitle.classList.add("hidden");
+				}
 			});
 		}
-
 		clearInput();
-		// watcher();
+	}
+};
+
+// Adding the task in list
+addButton.addEventListener("click", addTask);
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		addTask();
 	}
 });
-
-// if (tasksList) {
-// 	for (let i = 0; i < deleteButton.length; i++) {
-// 		deleteButton[i].addEventListener("click", function () {
-// 			console.log(deleteButton[i]);
-// 		});
-// 	}
-// }
